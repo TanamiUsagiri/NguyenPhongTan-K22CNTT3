02d861e307a727edf6ab1bfe74cc3b624ba22776
TanamiUsagiri <tanamiusagiri@gmail.com>
2024-03-28 11:20:52 +0700

    Lesson1

---
+++ hello/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NguyenPhongTan-K22CNTT3
+        </p>
+        <a
+          className="App-link"
+          href="https://daihocnguyentrai.edu.vn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Đại học Nguyễn Trãi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
